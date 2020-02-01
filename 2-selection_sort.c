f4d85207643e8c611aa02d441b712de1e0d7359d
TzStrikerYT <maicol.asus@gmail.com>
2020-01-31 19:09:16 -0500

    [add] adding second task, without BigOquiz

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t a, b, c;
+	int tmp;
+
+	if (!array || !size)
+		return;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		for (b = size - 1, c = a + 1; b > a; b--)
+		{
+			if (array[b] < array[c])
+				c = b;
+		}
+		if (array[a] > array[c])
+		{
+			tmp = array[a];
+			array[a] = array[c];
+			array[c] = tmp;
+			print_array(array, size);
+		}
+	}
+}
