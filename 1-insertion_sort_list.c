d5745774e76a097bef873df774e749d13a3b27e3
Michael <maicol.asus@gmail.com>
2020-02-03 21:50:59 +0000

    [fix] fix a condition

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,16 +10,15 @@
  * Return: Nothing
  */
 
-void list_swaper(listint_t *current, listint_t *next)
+void list_swaper(listint_t **list, listint_t *current, listint_t *next)
 {
 	if (current->prev)
-	{
 		current->prev->next = next;
-	}
 	if (next->next)
-	{
 		next->next->prev = current;
-	}
+	if (!current->prev)
+		*list = next;
+
 	current->next = next->next;
 	next->prev = current->prev;
 	current->prev = next;
@@ -41,14 +40,14 @@ void insertion_sort_list(listint_t **list)
 	{
 		if (front->next && front->n > front->next->n)
 		{
-			list_swaper(front, front->next);
+			list_swaper(list, front, front->next);
 			print_list(*list);
 			back = front->prev;
 			while (back)
 			{
 				if (back->prev && back->prev->n > back->n)
 				{
-					list_swaper(back->prev, back);
+					list_swaper(list, back->prev, back);
 					print_list(*list);
 				}
 				else
