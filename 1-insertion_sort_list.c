fa13a0573337a9c71e0ddcc79711f39290aacbd6
Michael <maicol.asus@gmail.com>
2020-02-03 22:16:17 +0000

    [fix] fix print condition

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -43,14 +43,14 @@ void insertion_sort_list(listint_t **list)
 		return;
 	while (front)
 	{
-		if (front->next && front->n >= front->next->n)
+		if (front->next && front->n > front->next->n)
 		{
 			list_swaper(list, front, front->next);
 			print_list(*list);
 			back = front->prev;
 			while (back)
 			{
-				if (back->prev && back->prev->n >= back->n)
+				if (back->prev && back->prev->n > back->n)
 				{
 					list_swaper(list, back->prev, back);
 					print_list(*list);
