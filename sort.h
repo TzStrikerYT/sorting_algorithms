c0b00c73534e565d6c99ce2f452bc7833ea81c2a
sebastianchc <1200@holbertonschool.com>
2020-02-04 13:55:16 -0500

    Header file

--- sort.h
+++ sort.h
@@ -36,6 +36,7 @@ void selection_sort(int *array, size_t size);
 void swaper(int *array, size_t size, int *a, int *b);
 void list_swaper(listint_t **list, listint_t *current, listint_t *next);
 void quick_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 /*size_t lomuto(int *array, size_t size , size_t low, size_t high);*/
 /*void sorter(int *array, size_t size, size_t low, size_t high);*/
 
